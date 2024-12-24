0eb75dd2e0d0e561d451e1dc419e3bd66b863897
johnabhishektg <john.tg177@gmail.com>
2024-12-25 00:07:45 +0530

    small changes

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,4 +1,4 @@
-import { Button, buttonVariants } from "@/components/ui/button";
+import { buttonVariants } from "@/components/ui/button";
 import { Icons } from "./icons";
 import Link from "next/link";
 import { cn } from "@/lib/utils";
